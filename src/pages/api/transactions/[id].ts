68a5781ab927f564ec7dcbd156a12e7c5b28eb52
Rohan Gayen <rohangayen@gmail.com>
2023-05-03 06:36:06 +0530

    feat: API for single tx

---
+++ src/pages/api/transactions/[id].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import transactionsDb from "@db/Parent.json";
+import installmentsDb from "@db/Child.json";
+import Installment from "@interfaces/installment";
+
+type Data = {
+  data: Installment[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id: transactionId } = req.query;
+  const { data: transactions } = transactionsDb;
+  const { data: installments } = installmentsDb;
+
+  const transactionDetails = transactions.find(
+    (tx) => tx.id === Number(transactionId)
+  );
+  const relevantInstallments = installments.filter(
+    (installment) => installment.parentId === Number(transactionId)
+  );
+
+  if (!transactionDetails) return res.status(200).json({ data: [] });
+  const result: Installment[] = relevantInstallments.map((installment) => ({
+    ...transactionDetails,
+    paidAmount: installment.paidAmount,
+  }));
+
+  res.status(200).json({ data: result });
+}
